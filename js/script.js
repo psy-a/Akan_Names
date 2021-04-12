7f0f3896f33d7febe2c75562f2862df3d5ae01ff
psy-a <psyfi488@gmail.com>
2021-04-12 19:40:35 +0300

    Fix getting day of week.

--- js/script.js
+++ js/script.js
@@ -76,10 +76,11 @@ function showAkanName(dateSelected, month, year, gender) {
 }
 
 function getDayOfWeek(day, month, year) {
+  
   var CC = parseInt(year.substring(0, 2));
   var YY = parseInt(year.substring(2, 4));
   var MM = parseInt(month);
   var DD = parseInt(day);
 
-  return (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
+  return ( DD + (((MM + 1) *26)/10) + YY + (YY/4) + 5 * (YY/100) + (YY/400) - 1) % 7;
 }
